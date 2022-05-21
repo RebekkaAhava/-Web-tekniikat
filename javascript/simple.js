8bb4527491b8dea960cbb9e8ac29b4c0711864d6
RebekkaAhava <rebekkaahava@gmail.com>
2022-05-21 17:19:33 +0300

    Subject change and task 3.1 updates and some css

--- javascript/simple.js
+++
@@ -1,7 +0,0 @@
-function button() {
-    if (document.getElementById("fname").value !== ""){
-    document.getElementById("welcome").innerHTML = `Tervetuloa, ${document.getElementById("fname").value}!`;
-} else {
-    return;
-}
-}
